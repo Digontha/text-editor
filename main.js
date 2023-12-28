06c44c90f91b97af485f514595c1efd92c3f0149
Digontha <digonthodj@gmail.com>
2023-12-28 20:12:12 +0600

    done

--- main.js
+++ main.js
@@ -8,3 +8,22 @@ function f2(e) {
     let frontValue = e.value
     textarea.style.color = frontValue ;
 }
+
+function f3(e) {
+    let fontValue = e.value;
+    textarea.style.fontFamily = fontValue;
+}
+
+function addNewPage() {
+    var existingTextarea = document.getElementById('text');
+    existingTextarea.parentNode.removeChild(existingTextarea);
+    var newTextarea = document.createElement("textarea");
+    newTextarea.placeholder = "Enter Your Text";
+    newTextarea.className = "textarea";
+    newTextarea.name = "textarea";
+    newTextarea.id = "text";
+    newTextarea.cols = "30";
+    newTextarea.rows = "10";
+    document.querySelector('.section').appendChild(newTextarea);
+}
+
